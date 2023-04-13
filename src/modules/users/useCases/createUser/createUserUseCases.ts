b581693949f1e5a553ed504acb5aa7418272980b
Wilhelm Steins <wilhelmsteins@gmail.com>
2023-04-12 23:55:33 -0300

    updating project prisma

--- src/modules/users/useCases/createUser/createUserUseCases.ts
+++ src/modules/users/useCases/createUser/createUserUseCases.ts
@@ -1,7 +1,28 @@
+import { prisma } from "../../../../prisma/client";
 import { CreateUserDTO } from "../../dtos/CreateUserDTO";
 
 export class CreateUserUseCase {
     async execute({ name, email }: CreateUserDTO) {
-        
+
+        // Verify that the user email already exists
+        const userAlreadyExists = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })  
+
+        if (userAlreadyExists) {
+            // error message
+        }
+
+        // Create the user
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email
+            }
+        })
+
+        return user;
     }
 }
