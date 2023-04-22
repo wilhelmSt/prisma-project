22e971c0577e63bedb03d23b262f76383caddcb6
Wilhelm Steins <wilhelmsteins@gmail.com>
2023-04-21 22:09:53 -0300

    Ajustando workspace e correção de bugs

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { CreateUserUseCase } from "./createUserUseCases";
+import { CreateUserUseCase } from "./CreateUserUseCases";
 
 export class CreateUserController {
     async handle(req: Request, res: Response) {
--- src/modules/users/useCases/createUser/createUserUseCases.ts
+++ src/modules/users/useCases/createUser/createUserUseCases.ts
@@ -1,19 +1,21 @@
-import { User } from '.prisma/client'
-import { prisma } from "../../../../prisma/client";
+import { User } from '@prisma/client'
 import { CreateUserDTO } from "../../dtos/CreateUserDTO";
+import { AppError } from '../../../../errors/AppError';
+import { prisma } from '../../../../prisma/client';
 
 export class CreateUserUseCase {
     async execute({ name, email }: CreateUserDTO): Promise<User> {
-
+        
         // Verify that the user email already exists
         const userAlreadyExists = await prisma.user.findUnique({
-            where: {
+            where: { 
                 email
             }
-        })  
+        })
 
         if (userAlreadyExists) {
-            // error message
+            // Error message
+            throw new AppError("User already exists!");
         }
 
         // Create the user
